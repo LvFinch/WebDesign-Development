61768790d8ad039e1268c3d04033861faed5ceab
Akko <33564554+LvFinch@users.noreply.github.com>
2020-10-26 22:58:29 +0800

    Update myscript.js

--- Lab5/E5/myscript.js
+++ Lab5/E5/myscript.js
@@ -1,16 +1,41 @@
 /*
  * @Author: Akko
  * @Date: 2020-10-17 16:09:13
- * @LastEditTime: 2020-10-18 17:51:23
+ * @LastEditTime: 2020-10-25 00:28:36
  * @LastEditors: Akko
  * @Description: 
  * @FilePath: /WebDesign-Development/Lab5/E5/myscript.js
  * @ Copyright ©️ 2020 Akko All rights reserved.
  */
-function Sum() {
-    var SumValue1 = document.getElementById("value1");
-    var SumValue2 = document.getElementById("value2");
-    var SumResult = document.getElementById("result");
-    
-    SumResult.value = parseInt(SumValue1) + parseInt(SumValue2);
+function Compute(comp) {
+    //计算符号判断
+    consele.log(comp.id);
+    var signid = comp.id;
+    alert(signid);
+    //获取两个数的值
+    value1 = Number(document.getElementById("value1").value);
+    value2 = Number(doucument.getElementById("value2").value);
+    //获取计算结果的值
+    result = document.getElementById("result").value;
+    //四则运算
+    switch (signid) {
+        case "Addition":
+            result = value1 + value2;
+            break;
+        case "Subtraction":
+            result = value1 - value2;
+            break;
+        case "Multiplication":
+            result = value1 * value2;
+            break;
+        case "Division":
+            if (value2 == 0) {
+                alert("除数不能为0!");
+                result = "error";
+            } else {
+                result = value1 / value2;
+            }
+            break;
+    }
+    document.getElementById("result").value = result;
 }
